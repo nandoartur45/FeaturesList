b6125e88e6fb3e6e93c9965596a46c335eecf1b3
Artur André <arturpossible@gmail.com.com.br>
2018-09-03 12:02:39 -0300

    Done: Unit Tests

--- tests/test.js
+++ tests/test.js
@@ -3,24 +3,37 @@ const item = "Rua A";
 
 QUnit.test("AddItem test", function(assert)
 {
-    itemsTest = new ItemsList()
+    let itemsTest = new ItemsList()
     itemsTest.addItem(item);
     assert.deepEqual(itemsTest.itemsArray, ["Rua A"], "The item was inserted in itemsArray.");
 })
 
 QUnit.test("RemoveItem test", function(assert)
 {
-    itemsTest = new ItemsList();
+    let itemsTest = new ItemsList();
     itemsTest.addItem(item);
-    assert.deepEqual(itemsTest.itemsArray, ["Rua A"], "itemsArray contains 'Rua A'.");
+    assert.deepEqual(itemsTest.itemsArray, ["Rua A"], "The item was inserted in itemsArray.");
     itemsTest.removeItem(item);
-    assert.equal(itemsTest.itemsArray.length, 0, "itemsArray is empty.");
+    assert.equal(itemsTest.itemsArray.length, 0, "The item was removed from itemsArray.");
 })
 
 QUnit.test("setItemActive test", function(assert)
 {
-    itemsTest = new ItemsList();
+    let itemsTest = new ItemsList();
     itemsTest.addItem(item);
-    itemsTest.setItemActive(itemId);
-    assert.deepEqual(itemsList.selectedItemsArray, ["Rua A"], "The 'active' class exists.");
+    assert.deepEqual(itemsTest.itemsArray, ["Rua A"], "The item was inserted in itemsArray.");
+    itemsTest.setItemActive(item);
+    assert.deepEqual(itemsTest.selectedItemsArray, ["Rua A"], "The item was inserted in selectedItemsArray (selected)");
+})
+
+QUnit.test("setItemInactive test", function(assert)
+{
+    let itemsTest = new ItemsList();
+    itemsTest.addItem(item);
+    assert.deepEqual(itemsTest.itemsArray, ["Rua A"], "The item was inserted in itemsArray.");
+    itemsTest.setItemActive(item);
+    assert.deepEqual(itemsTest.selectedItemsArray, ["Rua A"], "The item was inserted in selectedItemsArray (selected)");
+    itemsTest.setItemInactive(item);
+    assert.deepEqual(itemsTest.selectedItemsArray, [], "The item was removed from selectedItemsArray (unselected)");
+    assert.deepEqual(itemsTest.itemsArray, ["Rua A"], "The item stands in itemsArray.");
 })
